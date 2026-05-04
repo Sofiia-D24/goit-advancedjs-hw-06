4541d07a4f64253e3bb1ec208e7b11b2df942230
Sofiia-D24 <sofiiadiatlova@gmail.com>
2026-05-04 21:17:31 +0200

    Remove 'errors' field from Form type to create Params

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -1,15 +1,9 @@
-/*
-  У вас є тип Form, який містить інформацію про форму, включаючи поле errors. 
-  Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
-*/
-
 type Errors = {
   email?: string[];
   firstName?: string[];
   lastName?: string[];
   phone?: string[];
 };
-
 type Form = {
   email: string | null;
   firstName: string | null;
@@ -18,7 +12,5 @@ type Form = {
   errors: Errors;
 };
 
-// Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
-
+type Params = Omit<Form, 'errors'>;
 export {};
